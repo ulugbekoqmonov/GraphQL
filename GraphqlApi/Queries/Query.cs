772e9355ed4f7d59150973c60804d55520044a2d
ulugbekoqmonov <1.ulugbekoqmonov@gmail.com>
2023-08-11 18:17:58 +0500

    GetBooks added

--- GraphqlApi/Queries/Query.cs
+++ GraphqlApi/Queries/Query.cs
@@ -1,4 +1,5 @@
-﻿using GraphqlApi.Models;
+﻿using Bogus;
+using GraphqlApi.Models;
 
 namespace GraphqlApi.Queries;
 
@@ -16,4 +17,20 @@ public class Query
                 Email = "jonskeet@gamil.com"
             }
         };
+    public List<Book> GetBooks()
+    {
+        var faker = new Faker<Book>()
+            .RuleFor(b => b.Title, f => f.Lorem.Word())
+            .RuleFor(b => b.PageCount, f => f.Random.Int(100, 500))
+            .RuleFor(b => b.Description, f => f.Lorem.Sentence())
+            .RuleFor(b => b.Author, f => new Author
+            {
+                Name = f.Name.FullName(),
+                Age = f.Random.Int(30, 70),
+                Email = f.Person.Email
+            });
+
+        var books = faker.Generate(100);
+        return books;
+    }
 }
